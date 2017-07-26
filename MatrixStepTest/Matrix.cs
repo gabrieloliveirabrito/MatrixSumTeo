fb9522680d875bc4c6a8e96348f28d9671f2fc07
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2017-07-26 18:04:31 -0300

    Add files via upload

---
+++ MatrixStepTest/Matrix.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixStepTest
+{
+    public class Matrix
+    {
+        public static Matrix One => GetIdentity(1);
+        public static Matrix Identity2 => GetIdentity(2);
+        public static Matrix Identity3 => GetIdentity(3);
+
+        public static Matrix GetIdentity(int Dimension)
+        {
+            var M = new Matrix(Dimension, Dimension);
+            for (int x = 0; x < Dimension; x++)
+                M[x, x] = 1;
+            return M;
+        }
+
+        private double[][] Values;
+        public double this[int x, int y]
+        {
+            get => Values[x][y];
+            set => Values[x][y] = value;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Matrix(int X) : this(X, X) { }
+        public Matrix(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+
+            Values = new double[X][];
+            for (int x = 0; x < X; x++)
+            {
+                Values[x] = new double[Y];
+                for (int y = 0; y < Y; y++)
+                {
+                    Values[x][y] = 0;
+                }
+            }
+        }
+
+        public void SetAll(double Value)
+        {
+            for (int x = 0; x < X; x++)
+                for (int y = 0; y < Y; y++)
+                    Values[x][y] = Value;
+        }
+
+        public void Randomize(int Min = 0, int Max = 100)
+        {
+            var R = new Random((int)DateTime.Now.Ticks);
+            for (int x = 0; x < X; x++)
+            {
+                for (int y = 0; y < Y; y++)
+                {
+                    Values[x][y] = R.Next(Min, Max);
+                }
+            }
+        }
+
+        public Matrix Multiply(Matrix B, out int Count)
+        {
+            if (Y != B.X)
+                throw new InvalidOperationException();
+            else
+            {
+                var M = new Matrix(X, B.Y);
+                Count = 0;
+
+                for (int x = 0; x < X; x++)
+                {
+                    for (int y = 0; y < B.Y; y++)
+                    {
+                        for (int z = 0; z < Y; z++)
+                        {
+                            M[x, y] += this[x, z] * B[z, y];
+                            Count++;
+                        }
+                    }
+                }
+
+                return M;
+            }
+        }
+
+        public override string ToString()
+        {
+            var Result = string.Empty;
+            for (int x = 0; x < X; x++)
+            {
+                for (int y = 0; y < Y; y++)
+                {
+                    Result += $"{Values[x][y]}";
+                    if (y != Y - 1)
+                        Result += " ";
+                }
+
+                if (x != x - 1)
+                    Result += Environment.NewLine;
+            }
+            return Result;
+        }
+
+        public static Matrix operator *(Matrix A, Matrix B)
+        {
+            int Count = 0;
+            return A.Multiply(B, out Count);
+        }
+
+        public static Matrix operator *(Matrix A, double Scalar)
+        {
+            var M = new Matrix(A.X, A.Y);
+
+            for (int x = 0; x < A.X; x++)
+            {
+                for (int y = 0; y < A.Y; y++)
+                {
+                    M[x, y] = A[x, y] * Scalar;
+                }
+            }
+
+            return M;
+        }
+    }
+}
---
+++ MatrixStepTest/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MatrixStepTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Teo = new Func<Matrix, Matrix, int>((a, b) => a.X * a.Y * b.Y);
+
+            Console.Write("Digite o número de saídas para o aplicativo gerar: ");
+            for (int T = 1; T < int.Parse(Console.ReadLine()); T++)
+            {
+                Console.Write($"Try {T}...");
+                var FPath = Path.Combine(Environment.CurrentDirectory, $"Saida {T}.txt");
+                if (File.Exists(FPath))
+                    File.Delete(FPath);
+
+                var Old = Console.Out;
+                using (var Stream = File.Create(FPath))
+                {
+                    using (var Writer = new StreamWriter(Stream))
+                    {
+                        Console.SetOut(Writer);
+
+                        for (int x = 1; x <= T; x++)
+                        {
+                            for (int y = 1; y <= T; y++)
+                            {
+                                for (int z = 1; z <= T; z++)
+                                {
+                                    for (int w = 1; w <= T; w++)
+                                    {
+                                        Console.Title = $"n = {(x + 1) * (y + 1) * (z + 1) * (w + 1)}";
+                                        try
+                                        {
+                                            Console.Write($"Testing for A({x},{y})*B({z},{w})....");
+
+                                            Matrix A = new Matrix(x, y), B = new Matrix(z, w);
+                                            A.Randomize(1, 10);
+                                            B.Randomize(1, 10);
+
+                                            int Count;
+                                            var Result = A.Multiply(B, out Count);
+
+                                            Console.WriteLine($"Resultant A[{x},{y}]*B[{z},{w}] = C({Result.X},{Result.Y}), C = Teo | {Count} = {Teo(A, B)} == {Count == Teo(A, B)}");
+                                        }
+                                        catch (InvalidOperationException)
+                                        {
+                                            Console.WriteLine($"Non-valid matrix multiply dimensions (A[{x},{y}] * B[{z},{w}].");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    Console.SetOut(Old);
+                    Console.WriteLine($"End of {T} test!");
+                }
+            }
+
+            Console.WriteLine("End of all tests");
+            Console.ReadLine();
+        }
+    }
+}
